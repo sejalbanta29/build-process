35c6c11e20990b201866dff8875db977f665f5b5
sejalbanta29 <soodsejal5@gmail.com>
2021-04-07 23:43:40 +0530

    adding files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  hostURL: 'http://prod.site',
+  username: 'userProd1',
+  token:'abcxyzProd',
+  port:8080,
+  env_Name:'prod'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,15 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+
 export const environment = {
-  production: false
+  production: false,
+  hostURL: 'http://localhost',
+  username: 'user1',
+  token:'abcxyz',
+  port:8080,
+  env_Name:'Local'
+
 };
 
 /*
